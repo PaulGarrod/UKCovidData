21db01981f04476c77db928d82f38a36362130bd
Paul Garrod <58899433+PaulGarrod@users.noreply.github.com>
2020-11-15 17:46:51 +1300

    imported files

---
+++ covid19.js
@@ -0,0 +1,47 @@
+//setting todays date
+const todaysDate = document.querySelector("#currentDate")
+todaysDate.textContent = currentDay('-')
+document.querySelector('#todaysDate').innerHTML = `${currentDay('-')}`
+
+//selecting elements
+const totalCasesEl = document.querySelector("#totalCases")
+const todaysCasesEl = document.querySelector("#todaysCases")
+const totalDeathsEl = document.querySelector("#totalDeaths")
+const todaysDeathsEl = document.querySelector("#todaysDeaths")
+const todaysDateEl = document.querySelector("#todaysDate")
+//select options
+const selectDate = document.querySelector("#dates")
+
+//set the information for todays cases
+getTodaysData().then((data) => {
+    todaysCasesEl.innerHTML = data.cases.daily.toLocaleString()
+    totalCasesEl.innerHTML = data.cases.cumulative.toLocaleString()
+})
+//set the information for yesterdays deaths
+getYesterdaysData().then((data) => {
+    todaysDeathsEl.innerHTML = data.deaths.daily.toLocaleString()
+    totalDeathsEl.innerHTML = data.deaths.cumulative.toLocaleString()
+})
+
+//append a list of dates to the select options
+getData().then((data) => {
+    data.forEach((i) => {
+        const option = document.createElement('option')
+        option.innerHTML = (i.date)
+        document.querySelector('#dates').appendChild(option)
+    })
+})
+
+//when date option is selected update information to reflect selected date
+selectDate.addEventListener('change', () => {
+    //clear previous selected date information
+    clearSelectedDateData()
+    //create elements and import data from selected date
+    selectedData()
+})
+
+//Need to integrate this? 
+const highestDeathDay = getData().then((data) => {
+    let highestDeaths = Math.max.apply(Math, data.map (function (o) {return o.deaths.daily}))
+    return highestDeaths
+})
---
+++ requests.js
@@ -0,0 +1,149 @@
+// data API URL
+const covidURL = 'https://api.coronavirus.data.gov.uk/v1/data?' +
+'filters=areaType=nation;areaName=england&' +
+'structure={"date":"date","cases":{"daily":"newCasesByPublishDate","cumulative":"cumCasesByPublishDate"},"deaths":{"daily":"newDeathsByDeathDate","cumulative":"cumDeathsByDeathDate"},"tests":{"daily":"newTestsByPublishDate","cumulative":"cumTestsByPublishDate"},"deathsIn28Days":{"daily":"newDeaths28DaysByPublishDate","cumulative":"cumDeaths28DaysByPublishDate"},"hospitalCases":"hospitalCases"}'
+
+// get todats date
+const currentDay = function(sp){
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1;
+    let yyyy = today.getFullYear();
+    
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    return (yyyy+sp+mm+sp+dd.toString());
+}
+
+// get the data from the API
+const getData = async () => {
+    const response = await fetch (covidURL, {})
+
+    if (response.status === 200 || response.status === 204) {
+        const data = await response.json()
+        // console.log(data)
+        return data.data
+    } else {
+        throw new Error ('Whoops something went wrong with the respsonse!')
+    }
+}
+
+//data from most recent days entry
+const getTodaysData = async () => {
+    let todaysData = await getData()
+    todaysData = todaysData[0]
+    // console.log(todaysData)
+    return todaysData
+}
+
+//data from previous days entry (as deaths are not up dated on most recent day)
+const getYesterdaysData = async () => {
+    let yesterdaysData = await getData()
+    yesterdaysData = yesterdaysData[1]
+    return yesterdaysData
+}
+
+//create and show data for the selected date
+const selectedData = () => {
+    let selectedDate = selectDate.options[selectDate.selectedIndex].text
+    getData().then((data) => {
+        let dateObject = data.find ((i) => i.date === selectedDate)
+        
+        const selectedDateHeader = document.createElement('h2')
+        selectedDateHeader.innerHTML = (`Covid Data on ${selectedDate}`)
+        document.querySelector('#selectedDateHeader').appendChild(selectedDateHeader)
+
+        //case numbers on selected date
+        const casesCard = document.createElement('div')
+        document.querySelector('#covidCasesOnThisDate').appendChild(casesCard)
+        casesCard.className = 'card border-dark mb-3'
+        casesCard.id = 'casesCard'
+
+        const casesHeader = document.createElement('div')
+        document.querySelector('#casesCard').appendChild(casesHeader)
+        casesHeader.className = 'card-header'
+        casesHeader.innerHTML = 'Cases'
+
+        const casesBody = document.createElement('div')
+        document.querySelector('#casesCard').appendChild(casesBody)
+        casesBody.className = 'card-body text-dark'
+        casesBody.id = 'casesBody'
+
+        const casesTitle = document.createElement('div')
+        document.querySelector('#casesBody').appendChild(casesTitle)
+        casesTitle.className = 'card-title'
+        casesTitle.innerHTML = dateObject.cases.daily.toLocaleString()
+        
+        //death numbers on selected date
+        const deathsCard = document.createElement('div')
+        document.querySelector('#covidDeathsOnThisDate').appendChild(deathsCard)
+        deathsCard.className = 'card border-dark mb-3'
+        deathsCard.id = 'deathsCard'
+        
+        const deathsHeader = document.createElement('div')
+        document.querySelector('#deathsCard').appendChild(deathsHeader)
+        deathsHeader.className = 'card-header'
+        deathsHeader.innerHTML = 'Deaths'
+        
+        const deathsBody = document.createElement('div')
+        document.querySelector('#deathsCard').appendChild(deathsBody)
+        deathsBody.className = 'card-body text-dark'
+        deathsBody.id = 'deathsBody'
+        
+        const deathsTitle = document.createElement('div')
+        document.querySelector('#deathsBody').appendChild(deathsTitle)
+        deathsTitle.className = 'card-title'
+        deathsTitle.innerHTML = dateObject.deaths.daily.toLocaleString()
+        
+        //Total cases on selected date
+        const totalCasesCard = document.createElement('div')
+        document.querySelector('#totalCasesOnThisDate').appendChild(totalCasesCard)
+        totalCasesCard.className = 'card border-dark mb-3'
+        totalCasesCard.id = 'totalCasesCard'
+
+        const totalCasesHeader = document.createElement('div')
+        document.querySelector('#totalCasesCard').appendChild(totalCasesHeader)
+        totalCasesHeader.className = 'card-header'
+        totalCasesHeader.id = 'totalCasesHeader'
+        totalCasesHeader.innerHTML = 'Total Cases'
+
+        const totalCasesBody = document.createElement('div')
+        document.querySelector('#totalCasesCard').appendChild(totalCasesBody)
+        totalCasesBody.className = 'card-body text-dark'
+        totalCasesBody.id = 'totalCasesBody'
+
+        const totalCasesTitle = document.createElement('div')
+        document.querySelector('#totalCasesBody').appendChild(totalCasesTitle)
+        totalCasesTitle.className = 'card-title'
+        totalCasesTitle.innerHTML = dateObject.cases.cumulative.toLocaleString()
+      
+        //Total deaths on selected date
+        const totalDeathsCard = document.createElement('div')
+        document.querySelector('#totalDeathsOnThisDate').appendChild(totalDeathsCard)
+        totalDeathsCard.className = 'card border-dark mb-3'
+        totalDeathsCard.id = 'totalDeathsCard'
+        
+        const totalDeathsHeader = document.createElement('div')
+        document.querySelector('#totalDeathsCard').appendChild(totalDeathsHeader)
+        totalDeathsHeader.className = 'card-header'
+        totalDeathsHeader.innerHTML = 'Deaths'
+        
+        const totalDeathsBody = document.createElement('div')
+        document.querySelector('#totalDeathsCard').appendChild(totalDeathsBody)
+        totalDeathsBody.className = 'card-body text-dark'
+        totalDeathsBody.id = 'totalDeathsBody'
+        
+        const totalDeathsTitle = document.createElement('div')
+        document.querySelector('#totalDeathsBody').appendChild(totalDeathsTitle)
+        totalDeathsTitle.className = 'card-title'
+        totalDeathsTitle.innerHTML = dateObject.deaths.cumulative.toLocaleString()
+    })
+}
+
+const clearSelectedDateData = () => {
+    document.querySelector('#covidCasesOnThisDate').innerHTML = ''
+    document.querySelector('#totalCasesOnThisDate').innerHTML = ''
+    document.querySelector('#covidDeathsOnThisDate').innerHTML = ''
+    document.querySelector('#totalDeathsOnThisDate').innerHTML = ''
+    document.querySelector('#selectedDateHeader').innerHTML = ''
+}
